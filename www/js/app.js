90e5859315b321b81457896318d84b61a21b0c3e
Rafa P <kadetito@gmail.com>
2019-04-26 13:10:47 +0200

    drge
    ewfdf

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,9 @@
 
     /* ---------------------------------- Local Variables ---------------------------------- */
     var service = new EmployeeService();
+	
     service.initialize().done(function () {
+		this.$el.on('click', '.change-pic-btn', this.changePicture);
         console.log("Service initialized");
     });
 
@@ -25,5 +27,31 @@
             }
         });
     }
+
+
+this.changePicture = function(event) {
+  event.preventDefault();
+  if (!navigator.camera) {
+      alert("Camera API not supported", "Error");
+      return;
+  }
+  var options =   {   quality: 50,
+                      destinationType: Camera.DestinationType.DATA_URL,
+                      sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Album
+                      encodingType: 0     // 0=JPG 1=PNG
+                  };
+
+  navigator.camera.getPicture(
+      function(imgData) {
+          $('.media-object', this.$el).attr('src', "data:image/jpeg;base64,"+imgData);
+      },
+      function() {
+          alert('Error taking picture', 'Error');
+      },
+      options);
+
+  return false;
+};
+
 
 }());
