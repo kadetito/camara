863845d384fd64592dec6ef822877cf3c8f1ef2a
Rafa P <kadetito@gmail.com>
2019-04-26 13:15:47 +0200

    asx
    asx

--- www/js/index.js
+++ www/js/index.js
@@ -1,32 +1,95 @@
-function setOptions(srcType) {
-    var options = {
-        // Some common settings are 20, 50, and 100
-        quality: 50,
-        destinationType: Camera.DestinationType.FILE_URI,
-        // In this app, dynamically set the picture source, Camera or photo gallery
-        sourceType: srcType,
-        encodingType: Camera.EncodingType.JPEG,
-        mediaType: Camera.MediaType.PICTURE,
-        allowEdit: true,
-        correctOrientation: true  //Corrects Android orientation quirks
+var app = {
+  startCameraAbove: function(){
+    CameraPreview.startCamera({x: 50, y: 50, width: 300, height: 300, toBack: false, previewDrag: true, tapPhoto: true});
+  },
+
+  startCameraBelow: function(){
+    CameraPreview.startCamera({x: 50, y: 50, width: 300, height:300, camera: "front", tapPhoto: true, previewDrag: false, toBack: true});
+  },
+
+  stopCamera: function(){
+    CameraPreview.stopCamera();
+  },
+
+  takePicture: function(){
+    CameraPreview.takePicture(function(imgData){
+      document.getElementById('originalPicture').src = 'data:image/jpeg;base64,' + imgData;
+    });
+  },
+
+  switchCamera: function(){
+    CameraPreview.switchCamera();
+  },
+
+  show: function(){
+    CameraPreview.show();
+  },
+
+  hide: function(){
+    CameraPreview.hide();
+  },
+
+  changeColorEffect: function(){
+    var effect = document.getElementById('selectColorEffect').value;
+    CameraPreview.setColorEffect(effect);
+  },
+
+  changeFlashMode: function(){
+    var mode = document.getElementById('selectFlashMode').value;
+    CameraPreview.setFlashMode(mode);
+  },
+
+  changeZoom: function(){
+    var zoom = document.getElementById('zoomSlider').value;
+    document.getElementById('zoomValue').innerHTML = zoom;
+    CameraPreview.setZoom(zoom);
+  },
+
+  changePreviewSize: function(){
+    window.smallPreview = !window.smallPreview;
+    if(window.smallPreview){
+      CameraPreview.setPreviewSize({width: 100, height: 100});
+    }else{
+      CameraPreview.setPreviewSize({width: window.screen.width, height: window.screen.height});
     }
-    return options;
-}
+  },
 
-function openCamera(selection) {
+  showSupportedPictureSizes: function(){
+    CameraPreview.getSupportedPictureSizes(function(dimensions){
+      dimensions.forEach(function(dimension) {
+        console.log(dimension.width + 'x' + dimension.height);
+      });
+    });
+  },
 
-    var srcType = Camera.PictureSourceType.CAMERA;
-    var options = setOptions(srcType);
-    var func = createNewFileEntry;
+  init: function(){
+    document.getElementById('startCameraAboveButton').addEventListener('click', this.startCameraAbove, false);
+    document.getElementById('startCameraBelowButton').addEventListener('click', this.startCameraBelow, false);
 
-    navigator.camera.getPicture(function cameraSuccess(imageUri) {
+    document.getElementById('stopCameraButton').addEventListener('click', this.stopCamera, false);
+    document.getElementById('switchCameraButton').addEventListener('click', this.switchCamera, false);
+    document.getElementById('showButton').addEventListener('click', this.show, false);
+    document.getElementById('hideButton').addEventListener('click', this.hide, false);
+    document.getElementById('takePictureButton').addEventListener('click', this.takePicture, false);
+    document.getElementById('selectColorEffect').addEventListener('change', this.changeColorEffect, false);
+    document.getElementById('selectFlashMode').addEventListener('change', this.changeFlashMode, false);
 
-        displayImage(imageUri);
-        // You may choose to copy the picture, save it somewhere, or upload.
-        func(imageUri);
+    if(navigator.userAgent.match(/Android/i)  == "Android"){
+      document.getElementById('zoomSlider').addEventListener('change', this.changeZoom, false);
+    }else{
+      document.getElementById('androidOnly').style.display = 'none';
+    }
 
-    }, function cameraError(error) {
-        console.debug("Unable to obtain picture: " + error, "app");
+    window.smallPreview = false;
+    document.getElementById('changePreviewSize').addEventListener('click', this.changePreviewSize, false);
 
-    }, options);
-}
+    document.getElementById('showSupportedPictureSizes').addEventListener('click', this.showSupportedPictureSizes, false);
+
+    // legacy - not sure if this was supposed to fix anything
+    //window.addEventListener('orientationchange', this.onStopCamera, false);
+  }
+};
+
+document.addEventListener('deviceready', function(){	
+  app.init();
+}, false);
