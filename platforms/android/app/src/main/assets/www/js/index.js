b0d1d1c3dec80b59c29d010b7254fd9a8ec6904a
Rafa P <kadetito@gmail.com>
2019-04-26 13:01:47 +0200

    dtge
    erg

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,40 +1,32 @@
-var app = {
- 
-          initialize: function() {
-              this.bindEvents();
-          },
-         
-          bindEvents: function() {
-              var takePhoto = document.getElementById('takePhoto');
-              takePhoto.addEventListener('click', app.takePhoto, false);
-              var sendPhoto = document.getElementById('sendPhoto');
-              sendPhoto.addEventListener('click', app.sendPhoto, false);
-          },
- 
-          sendPhoto: function() {
-              alert('Imagen enviada al servidor');
-          },
- 
-          takePhoto: function(){
-              navigator.camera.getPicture(app.onPhotoDataSuccess, app.onFail, { quality: 20, 
-                  allowEdit: true, destinationType: navigator.camera.DestinationType.DATA_URL });
-          },
- 
-          onPhotoDataSuccess: function(imageData) {
-         
-            var photo = document.getElementById('photo');
- 
-            photo.style.display = 'block';
- 
-            photo.src = "data:image/jpeg;base64," + imageData;
- 
-            var sendPhoto = document.getElementById('sendPhoto');
-            sendPhoto.style.display = 'block';
-            
-          },
- 
-          onFail: function(message) {
-            alert('Failed because: ' + message);
-          }
- 
-      };
+function setOptions(srcType) {
+    var options = {
+        // Some common settings are 20, 50, and 100
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery
+        sourceType: srcType,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: Camera.MediaType.PICTURE,
+        allowEdit: true,
+        correctOrientation: true  //Corrects Android orientation quirks
+    }
+    return options;
+}
+
+function openCamera(selection) {
+
+    var srcType = Camera.PictureSourceType.CAMERA;
+    var options = setOptions(srcType);
+    var func = createNewFileEntry;
+
+    navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+        displayImage(imageUri);
+        // You may choose to copy the picture, save it somewhere, or upload.
+        func(imageUri);
+
+    }, function cameraError(error) {
+        console.debug("Unable to obtain picture: " + error, "app");
+
+    }, options);
+}
